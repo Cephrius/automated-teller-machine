8b2ff913776ca671cd908f19c020be6dc0a08084
Chiedozie Ehileme <ehilemece@hbu.edu>
2023-06-10 01:21:58 -0500

    230600 added dict of account info soon to implement

--- GUI/main.py
+++ GUI/main.py
@@ -5,19 +5,66 @@
 ########################################################################################################################
 
 # stores all login info acts as 
-accNumbers = {
-              "2241323":'2214'   # <--- Number 1
-              ,"9388292":'5516'  # <--- Number 2
-              ,"2856291":'0291'  # <--- Number 3
-              ,"560101":'6373'   # <--- Number 4
-              ,"5429910":'4827'  # <--- Number 5
-              ,"2851798":'9271'} # <--- Number 6
+account1 = { # <----- Account 1 
+    "accountName":   "Tanya Wilson",
+    "accountNumber":  "2241323",
+    "accountPin":     2214
+}
+
+account2 = {
+    "accountName":   "John Smith",
+    "accountNumber": "9388292",
+    "accountPin" :    5516
+}
+
+account3 = {
+    "accountName":    "Laura Johnson",
+    "accountNumber":  "2856291",
+    "accountPin" :     2617
+}
+
+account4 = {
+    "accountName":   "Micheal Davis",
+    "accountNumber": "560101",
+    "accountPin" :    6373
+}
+
+account5 = {
+    "accountName":   "Emily Anderson",
+    "accountNumber": "5429910",
+    "accountPin" :    4827
+}
+
+account6 = {
+    "accountName":   "Christpher Thompson",
+    "accountNumber": "2851798",
+    "accountPin" :    9271
+}
+
+accInfo = {
+    "account1": account1,
+    "account2": account2,
+    "account3": account3,
+    "account4": account4,
+    "account5": account5,
+    "account6": account6
+
+}
 
 
+def checkingAccount():
+    print("######################## Welcome to your Checking Account!! ###########################")
+    print("Account Number: " +accountNumber)
+    print()
+    
+
+def savingsAccount():
+    pass
 
 def mainScreen():
     print()
     print()
+    print("************************Succsfully Logged in***************************") # <----- Prints when incorrect pin number is inputed
     print("######################## Welcome to your account!! ###########################")
     print("Account Number: " +accountNumber)
     print()
@@ -25,38 +72,50 @@ def mainScreen():
     print("1. Checking Account")
     print("2. Savings Account")
     print()
+    print()
+    print()
+    print("Log Out")
     print("###############################################################################")
     select = input()
     
+    if select == "1":
+        checkingAccount()
+    elif select == "2":
+        savingsAccount()
+    elif select.casefold() == "Log Out ":
+        Login()
 
 
 
+### <----- To authenticate user
+accountNumber = input("Enter you account number: ")
 while True:
-    accountNumber = input("Enter your account number: ")
-    if accountNumber in accNumbers:
-        pin = input("Enter your pin: ")
-        if pin in accNumbers[accountNumber]:
-            print("************************Succsfully Logged in***************************")
+    
+    if accountNumber == accInfo["accountNumber"]:
+        accountPin = input("Enter your pin: ")
+        if accountPin in accInfo["accountPin"]:
             mainScreen()
-            break # exits the loop since login is successful
-        else:
-            print("########################## INCORRECT PIN #############################")# <----- Prints when incorrect pin number is inputed
+            break
+        else: 
+            print("########################## INCORRECT PIN NUMBER #############################") # <----- Prints when incorrect account number is inputed
             
     else:
-        print("########################## INCORRECT ACCOUNT NUMBER #############################")  # <----- Prints when incorrect account number is inputed
-        
-        
-        
-        
+        print("########################## INCORRECT ACCOUNT NUMBER #############################")# <----- Prints when incorrect pin number is inputed
+        accountNumber = input("Enter you account number: ")
+
+    
+    
   
 
 
 
 
 
+    
+    
 
-
-
+      
+    
 
 
 
